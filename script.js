95c65336f14dff800d91bde545d77499ea7ed568
mostafamohammadi1989 <mostafa.mohammadi.1989.10.26@gmail.com>
2023-02-21 02:41:30 +0330

    create form and table and write some of function

--- script.js
+++ script.js
@@ -1,33 +1,39 @@
-var form = document.querySelector("form");
-var table = document.querySelector("table tbody");
+// var button = document.getElementById("submit");
+// var username = document.getElementById("username");
+// var password = document.getElementById("password");
+// var email = document.getElementById("email");
+// var tbody = document.getElementById("tbody");
 
-function addRow() {
-  let username = document.querySelector("#username").value;
-  let password = document.querySelector("#password").value;
-  let email = document.querySelector("#email").value;
+// button.addEventListener("click", function () {
+//   var tr = document.createElement("tr");
+//   var td = document.createElement("td");
+//   td.appendChild(document.createTextNode(username.value));
+//   tr.appendChild(td);
+//   tbody.appendChild(tr);
+// });
 
-  let row = document.createElement("tr");
-  let cellUsername = document.createElement("td");
-  let cellPassword = document.createElement("td");
-  let cellEmail = document.createElement("td");
-  let cellDelet = document.createElement("td");
-  let deletBtn = document.createElement("button");
+var button = document.getElementById("submit");
 
-  cellUsername.textContent = username;
-  cellPassword.textContent = password;
-  cellEmail.textContent = email;
-  deletBtn.textContent = "Delet";
+button.addEventListener("click", function () {
+  const username = document.getElementById("username");
+  const password = document.getElementById("password");
+  const email = document.getElementById("email");
 
-  deletBtn.addEventListener("click", () => {
-    row.remove();
-  });
+  const tbody = document
+    .getElementById("table")
+    .getElementsByTagName("tbody")[0];
 
-  cellDelet.appendChild(deletBtn);
-  row.appendChild(cellUsername);
-  row.appendChild(cellPassword);
-  row.appendChild(cellEmail);
-  row.appendChild(cellDelet);
-  table.appendChild(row);
-}
+  const newRow = tbody.insertRow();
 
-document.querySelector("#submit").addEventListener("click", addRow);
+  const usernameCell = newRow.insertCell();
+  const passwordCell = newRow.insertCell();
+  const emailCell = newRow.insertCell();
+
+  const usernameText = document.createTextNode(username.value);
+  const passwordText = document.createTextNode(password.value);
+  const emailText = document.createTextNode(email.value);
+
+  usernameCell.appendChild(usernameText);
+  passwordCell.appendChild(passwordText);
+  emailCell.appendChild(emailText);
+});
